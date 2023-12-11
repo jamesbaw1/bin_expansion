8b18f0494bf4d38e06446f134d4eb34b013f4143
jamesbaw1 <jamiebaw@gmail.com>
2023-12-11 18:22:14 +0000

    adds new struct `Binomial` which streamlines functionality

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,83 @@
-// use num_bigint::BigUint;
-use std::fmt;
+use num_bigint::BigInt;
 use std::time::Instant;
+// use std::fmt;
 
 #[derive(Debug)]
 struct Term {
-    coefficient: i32,
-    symbol: char,
-    exponent: u32,
+    coeff: BigInt,
+    a_exp: u32,
+    b_exp: u32,
+}
+
+trait Eval {
+    fn eval(&self, a: BigInt, b: BigInt) -> BigInt;
 }
 
 impl Term {
-    fn new(symbol: char) -> Self {        
+    fn from(coeff: BigInt, a_exp: u32, b_exp: u32) -> Self {
         Term {
-            coefficient: 1,
-            symbol,
-            exponent: 1, 
+            coeff,
+            a_exp,
+            b_exp,
+        }
+    }
+}
+
+impl Eval for Vec<Term> {
+    fn eval(&self, a: BigInt, b: BigInt) -> BigInt {
+        self
+            .iter()
+            .fold(BigInt::from(0u32), |acc, x| acc + &x.coeff * a.pow(x.a_exp) * b.pow(x.b_exp))
+    }
+}
+
+#[derive(Debug)]
+struct Binomial {
+    a_coeff: BigInt,
+    a_exp: u32,
+    b_coeff: BigInt,
+    b_exp: u32,
+    n: u32,
+}
+
+impl Binomial {
+    fn new() -> Self {
+        
+        Binomial {
+            a_coeff: BigInt::from(1u32),
+            a_exp: 1,
+            b_coeff: BigInt::from(1u32),
+            b_exp: 1,
+            n: 1,
         }
     }
 
-    fn from(coefficient: i32, symbol: char, exponent: u32) -> Self {        
-        Term {
-            coefficient,
-            symbol,
-            exponent, 
+    fn from(a_coeff: BigInt, a_exp: u32, b_coeff: BigInt, b_exp: u32, n: u32) -> Self {
+        Binomial {
+            a_coeff,
+            a_exp,
+            b_coeff,
+            b_exp,
+            n,
         }
     }
 
-    fn eval(&self, value: i32) -> i32 {
-        self.coefficient * value.pow(self.exponent)
+    fn expand(&self) -> Vec<Term> {
+        let mut result = Vec::new();
+        for i in 0..=self.n {
+            result.push(
+                Term::from(
+                    BigInt::from(choose(self.n, i)) * self.a_coeff.pow(self.n - i) * self.b_coeff.pow(i),
+                    self.a_exp * (self.n - i),
+                    self.b_exp * i,
+            ));
+        }
+
+        result
     }
 }
 
-impl fmt::Display for Term {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{}{}^{}",
-            if self.coefficient != 1 {
-                format!("{}", self.coefficient)
-            } else {
-                String::new()
-            },
-            self.symbol,
-            self.exponent
-        )
-    }
-}
-
-fn expand(a: Term, b: Term, n: u32) -> Vec<(i32, Term, Term)> {
-    let mut binomial = Vec::new();
-    for x in 0..=n {
-        let y = n-x;
-        let c = choose(n, x);
-        binomial.push(
-            (
-                c*a.coefficient.pow(y)*b.coefficient.pow(x),
-                Term::from(1, 'a', a.exponent*y),
-                Term::from(1, 'b', b.exponent*x)
-            )
-        );
-    }
-
-    binomial
-}
-
-fn substitute(binomial: Vec<(i32, Term, Term)>, a: i32, b: i32) -> i32 {
-    binomial
-    .iter()
-    .fold(0, |acc, x| acc + x.0 * x.1.eval(a) * x.2.eval(b))
-}
-
-fn choose(n: u32, r: u32) -> i32 {
+fn choose(n: u32, r: u32) -> u32 {
     let (a, b) = if r > (n-r) {
         (r, n-r)
     } else {
@@ -83,27 +90,27 @@ fn choose(n: u32, r: u32) -> i32 {
     let divisor = (2..=b)
         .fold(1, |acc, i| acc * i);
 
-    (dividend/divisor) as i32
+    dividend/divisor
 }
 
-
 fn main() {
-    // let start = Instant::now();
-    // let result = pascal(5);
-    // let duration = start.elapsed();
-    // println!("{:.2?}\n{:?}", duration, result);
+    let a_coeff = BigInt::from(-12i32);
+    let a_exp = 4;
+    let b_coeff = BigInt::from(23i32);
+    let b_exp = 7;
+    let n = 15;
 
-    let a = Term::from(2, 'a', 3);
-    let b = Term::new('b');
-    let n = 3;
+    let binomial = Binomial::from(a_coeff, a_exp, b_coeff, b_exp, n);
 
-    println!("({} + {})^{}", &a, &b, &n);
+    println!("{:?}", &binomial);
 
     let start = Instant::now();
-    let result = expand(a, b, n);
+    let result = binomial.expand();
     let duration = start.elapsed();
 
-    println!("{:.2?}", duration);
+    let r = result.eval(BigInt::from(2i32), BigInt::from(2i32));
 
-    println!("{}", substitute(result, 2, 12))
+    println!("{:?}", r);
+
+    println!("{:.2?}", duration);
 }
